57e01d42782edaae56f267d2666aec28a63aea03
WooJin <zathy1225@gmail.com>
2023-02-20 22:58:40 +0900

    classes & constructor

---
+++ Class/basicClasses.dart
@@ -0,0 +1,44 @@
+class Player {
+  final String name;
+  int xp, age;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
+  void sayHello() {
+    print('my name is $name');
+  }
+}
+
+void main(List<String> args) {
+  var player = Player.createBluePlayer(
+    name: 'woojin',
+    age: 25,
+  );
+
+  var player2 = Player.createRedPlayer(
+    'woojin',
+    25,
+  );
+
+  player.sayHello();
+}
